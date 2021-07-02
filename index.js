6b76927e9b5db9d9bb552fa6626f63ac72657902
SebastianMacaya <contacto@sebastianmacaya.com>
2021-07-02 16:18:10 -0300

    agrego documentacion al script

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const initProductos = () => {
+  //inicializo mis variables
   let precioDeLaCompra = 0;
   let cantidadHardware;
   const listaHardware = [];
 
+  //Obtengo elementos del DOM con los ID
   const formCantidadHardware = document.getElementById("formCantidadHardware");
   const inputCantidadHardware = document.getElementById("cantidadHardware");
   const cantidadSeleccionada = document.getElementById("cantidadSeleccionada");
@@ -14,33 +16,39 @@ const initProductos = () => {
 
   const resultadoDeLaCompra = document.getElementById("resultadoDeLaCompra");
 
+  //oculto el form de crear productos
   formCrearProducto.style.display = "none";
 
+  //AGREGO EVENTO AL ENVIAR CANTIDAD DE HARDWARE A CREAR
   formCantidadHardware.addEventListener("submit", (e) => {
+    //evito el comportamiento normal del submit
     e.preventDefault();
 
+    //asigno el valor numerico a cantidad de hardware
     cantidadHardware = Number(inputCantidadHardware.value);
 
+    //oculto el form de la cantidad de hardware
     formCantidadHardware.style.display = "none";
 
     cantidadSeleccionada.textContent = `Has seleccionado ${cantidadHardware} hardwares`;
     formCrearProducto.style.display = "block";
   });
 
+  //AGREGO EVENTO AL ENVIAR LOS PRODUCTOS
   formCrearProducto.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const marca = inputMarca.value;
     const modelo = inputModelo.value;
     const precio = inputPrecio.value;
-
+    //guardo el producto en el array
     const producto = { marca, modelo, precio };
     listaHardware.push(producto);
 
     inputMarca.value = "";
     inputModelo.value = "";
     inputPrecio.value = "";
-
+    //oculto el form una vez que se cumpla la cantidad a agregar
     if (listaHardware.length === cantidadHardware) {
       formCrearProducto.style.display = "none";
       crearProductos();
