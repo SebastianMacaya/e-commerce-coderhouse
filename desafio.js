bd4a14688583024db531612a2b82c0372b95785d
SebastianMacaya <contacto@sebastianmacaya.com>
2021-07-13 17:58:26 -0300

    empiezo a crear carrito de compras de 0

--- desafio.js
+++ desafio.js
@@ -1,88 +1,32 @@
-const initProductos = () => {
-  //inicializo mis variables
-  let precioDeLaCompra = 0;
-  let cantidadHardware;
-  const listaHardware = [];
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
 
-  //Obtengo elementos del DOM con los ID
-  const formCantidadHardware = document.getElementById("formCantidadHardware");
-  const inputCantidadHardware = document.getElementById("cantidadHardware");
-  const cantidadSeleccionada = document.getElementById("cantidadSeleccionada");
-
-  const formCrearProducto = document.getElementById("crearProducto");
-  const inputMarca = document.getElementById("inputMarca");
-  const inputModelo = document.getElementById("inputModelo");
-  const inputPrecio = document.getElementById("inputPrecio");
-
-  const resultadoDeLaCompra = document.getElementById("resultadoDeLaCompra");
-
-  //oculto el form de crear productos
-  formCrearProducto.style.display = "none";
-
-  //AGREGO EVENTO AL ENVIAR CANTIDAD DE HARDWARE A CREAR
-  formCantidadHardware.addEventListener("submit", (e) => {
-    //evito el comportamiento normal del submit
-    e.preventDefault();
-
-    //asigno el valor numerico a cantidad de hardware
-    cantidadHardware = Number(inputCantidadHardware.value);
-
-    //oculto el form de la cantidad de hardware
-    formCantidadHardware.style.display = "none";
-
-    cantidadSeleccionada.textContent = `Has seleccionado ${cantidadHardware} hardwares`;
-    formCrearProducto.style.display = "block";
-  });
-
-  //AGREGO EVENTO AL ENVIAR LOS PRODUCTOS
-  formCrearProducto.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const marca = inputMarca.value;
-    const modelo = inputModelo.value;
-    const precio = inputPrecio.value;
-    //guardo el producto en el array
-    const producto = { marca, modelo, precio };
-    listaHardware.push(producto);
-
-    inputMarca.value = "";
-    inputModelo.value = "";
-    inputPrecio.value = "";
-    //oculto el form una vez que se cumpla la cantidad a agregar
-    if (listaHardware.length === cantidadHardware) {
-      formCrearProducto.style.display = "none";
-      crearProductos();
-    }
-  });
-
-  const crearProductos = () => {
-    for (const producto of listaHardware) {
-      crearProducto(producto);
-    }
-  };
-  //Creo los productos usando la template
-  const crearProducto = (producto) => {
-    const template = document.getElementById("producto");
-    const productoElement = template.content.cloneNode(true);
-
-    productoElement.getElementById(
-      "nombre"
-    ).textContent = `Marca: ${producto.marca}`;
-    productoElement.getElementById(
-      "modelo"
-    ).textContent = `Modelo: ${producto.modelo}`;
-    productoElement.getElementById(
-      "precio"
-    ).textContent = `$ ${producto.precio}`;
-    //Sumo la cantidad de productos comprados
-    productoElement.getElementById("comprar").addEventListener("click", () => {
-      precioDeLaCompra += Number(producto.precio);
-
-      resultadoDeLaCompra.textContent = `Lleva usted gastados $${precioDeLaCompra}`;
-    });
-
-    document.body.appendChild(productoElement);
-  };
+const fetchData = async () => {
+  try {
+    const res = await fetch("api.json");
+    const data = await res.json();
+    pintarCards(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-initProductos();
+const pintarCards = (data) => {
+  console.log(data);
+  data.forEach((producto) => {
+    console.log(producto.title);
+    templateCard.querySelector("h5").textContent = producto.title;
+    templateCard.querySelector("p").textContent = producto.precio;
+    templateCard
+      .querySelector("img")
+      .setAttribute("src", producto.thumbnailUrl);
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+
+  items.appendChild(fragment);
+};
